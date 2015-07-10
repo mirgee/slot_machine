a756833d291939cb425704c38b3becdec76fc178
mirgee <mrslvkvr@gmail.com>
2015-07-10 22:07:57 +0200

    Added some comments and a new file

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/* I will add some stupid multiline comment here on purpuose, just to try out
+   git diff features and stuff.
+ */
+
+// Main class of the project - stupid comment, delete on next commit
 public class Main {
 
     public static void main(String[] args) {
