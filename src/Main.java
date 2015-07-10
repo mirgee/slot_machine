1e3a6b3cf1ad1eb10040b9b7336a418a7b8b596f
miroslav <mrslvkvr@gmail.com>
2015-07-10 22:01:55 +0200

    Added some TODOs

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    private enum Reel { CHERRIES, ORANGES, PLUMS, BELLS, MELONS, BARS }
+
+    private static final int numOfReels = 3;
 
     public static void main(String[] args) {
 
+        double currBet;
+        String response;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter the amount of money you want to insert into the slot machine: ");
+
+            currBet = keyboard.nextDouble();
+
+            // Generate randomReels
+
+            // Print result
+
+            // Determine the profit and add it to amountWon
+
+            // Ask user if he wants to continue
+            System.out.print("Do you want to continue playing? (Y / N)");
+            response = keyboard.next();
+        } while (response.equals("Y") || response.equals("y"));
+
+        // Print the final amountWon
     }
 }
