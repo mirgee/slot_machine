ee203de2ace9d95dca34d866614d3479ff930719
miroslav <mrslvkvr@gmail.com>
2015-07-12 10:47:41 +0200

    Added some test signatures

---
+++ test/ReelTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReelTest {
+
+    @Test
+    public void testGenerateRandomReel() throws Exception {
+
+    }
+
+    @Test
+    public void testCalculateProfitPercentage() throws Exception {
+
+    }
+}
