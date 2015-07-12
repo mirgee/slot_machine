21d5a22ac9d59badb2e324f05cf18f8210f2e6ef
mirgee <mrslvkvr@gmail.com>
2015-07-12 13:54:52 +0200

    Added some tests for calculating percentages

--- test/ReelTest.java
+++ test/ReelTest.java
@@ -1,10 +1,10 @@
 import org.junit.Test;
 
 import java.lang.Exception;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
-
-import Reel.*;
 
 public class ReelTest {
 
@@ -15,20 +15,32 @@ public class ReelTest {
 
     @Test
     public void testGetCherries() throws Exception {
-        ReelType cherry = new ReelType("Cherries", 0);
-        assertEqals(0, cherry.getId());
+        Reel.ReelType cherry = Reel.ReelType.CHERRIES;
+        assertEquals(0, cherry.getId());
         assertEquals("Cherries", cherry.toString());
     }
 
     @Test
-    public void testGetNonexistent throws Exception {
-        ReelType banana = new ReelType("Banana", 20);
-        assertEquals(20, banana.getId());
-        assertEquals("Banana", banana.toString());
+    public void testCalculateProfitPercentageOne() throws Exception {
+        List<Reel.ReelType> strings = new ArrayList<Reel.ReelType>();
+        strings.add(Reel.ReelType.BARS);
+        strings.add(Reel.ReelType.BARS);
+        strings.add(Reel.ReelType.CHERRIES);
+        assertEquals(2, Reel.calculateProfitPercentage(strings));
     }
 
     @Test
-    public void testCalculateProfitPercentage() throws Exception {
+    public void testCalculateProfitPercentageZero() throws Exception {
+        List<Reel.ReelType> strings = new ArrayList<Reel.ReelType>();
+        assertEquals(0, Reel.calculateProfitPercentage(strings));
+    }
 
+    @Test
+    public void testCalculateProfitPercentageTwo() throws Exception {
+        List<Reel.ReelType> strings = new ArrayList<Reel.ReelType>();
+        strings.add(Reel.ReelType.BARS);
+        strings.add(Reel.ReelType.BARS);
+        strings.add(Reel.ReelType.BARS);
+        assertEquals(3, Reel.calculateProfitPercentage(strings));
     }
 }
